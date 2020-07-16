6517b53ac0078e27ae0f94cd29fe96df394b9d31
Henry Liu <hl3184@columbia.edu>
2020-07-16 11:47:17 -0400

    small fix

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 //DOM
-const container = $("data_container");
-const mainButton = $(".all_btn");
-const mealButton = $("#meal_btn");
-const testingButton = $("#testing_btn");
-const userInput = $("#user_input");
-const form = $("form");
+const container = $('#data_container');
+const mainButton = $('.all_btn');
+const mealButton = $('#meal_btn');
+const testingButton = $('#testing_btn');
+const userInput = $('#user_input');
+const form = $('form');
 
 //Event Listener
-form.on("submit", function (e) {
+form.on('submit', function (e) {
   e.preventDefault();
 });
 
-mainButton.on("click", function () {
+mainButton.on('click', function () {
   getTestingLocation();
   getFreeMeals();
 });
 
-testingButton.on("click", function () {
+testingButton.on('click', function () {
+  console.log('clicked');
   getTestingLocation();
 });
 
-mealButton.on("click", function () {
+mealButton.on('click', function () {
   getFreeMeals();
 });
 
@@ -29,17 +30,17 @@ mealButton.on("click", function () {
 //Function to get and render testing location
 function getTestingLocation(e) {
   // Get value of typed in zip, store in a variable.
-  var enteredZip = parseInt($("#user_input").val());
-  // console.log(enteredZip);
+  var enteredZip = parseInt($('#user_input').val());
+  console.log(enteredZip);
   // Empty the container of previously displayed cards
   container.empty();
   // This is the API link for testing locations.
   var queryURL =
-    "https://covid-19-testing.github.io/locations/new-york/complete.json";
+    'https://covid-19-testing.github.io/locations/new-york/complete.json';
   // Create an AJAX call to retrieve data
   $.ajax({
     url: queryURL,
-    method: "GET",
+    method: 'GET',
   }).then(function (response) {
     // Run a loop that looks at every zip code in the API array.
     for (let i = 0; i < response.length; i++) {
@@ -53,37 +54,37 @@ function getTestingLocation(e) {
         // console.log(response);
         // console.log(response[i].physical_address[0].postal_code);
         // Generate/Fill cards:
-        $("<div>", {
-          class: "cell",
+        $('<div>', {
+          class: 'cell',
         })
           .append(
-            $("<div>", {
-              class: "card",
+            $('<div>', {
+              class: 'card',
             }).append(
-              $("<div>", {
-                class: "card-selection",
+              $('<div>', {
+                class: 'card-selection',
               }).append(
-                $("<h2>", {
-                  class: "#",
+                $('<h2>', {
+                  class: '#',
                   text: `Cite Name: ${response[i].alternate_name}`,
                 }),
-                $("<p>", {
+                $('<p>', {
                   text: `Cite Location: ${response[i].physical_address[0].address_1}`,
                 }),
-                $("<p>", {
-                  class: "card-text",
+                $('<p>', {
+                  class: 'card-text',
                   html: `Hours: `,
                   // Need to address this, sometimes hours are not provided.
                   // html: `Hours: ${response[i].regular_schedule[0].opens_at} - ${response[i].regular_schedule[0].closes_at}`,
                 }),
-                $("<p>", {
-                  class: "card-text",
+                $('<p>', {
+                  class: 'card-text',
                   html: `Phone Number: ${response[i].phones[0].number}`,
                 })
               )
             )
           )
-          .appendTo($("#data_container"));
+          .appendTo($('#data_container'));
       }
     }
   });
